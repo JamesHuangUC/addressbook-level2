c971ffccd889e3e80bb837ac8d59a17f16fd50c8
Zihua Huang <jameshuanguc@gmail.com>
2016-09-09 13:24:32 +0800

    fixed adding comment to Contact and change value to public

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -11,7 +11,6 @@ public class Address extends Contact{
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
-
     public final String value;
 
     /**
